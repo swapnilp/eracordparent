e7e4ec3f0c23cc6a47afa161518f738f08094cd8
SwapnilPatil <swapnil.patil04@gmail.com>
2017-10-23 08:30:00 +0530

    work on UI

--- src/pages/exams/exams.ts
+++ src/pages/exams/exams.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { MpinLoginPage } from '../mpin-login/mpin-login';
 
 @Component({
   selector: 'page-exams',
