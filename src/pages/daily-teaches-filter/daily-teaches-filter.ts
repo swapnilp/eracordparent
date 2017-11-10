93361e3acb8b8a7f3d5a4389b8e238c3ead63a6d
SwapnilPatil <swapnil.patil04@gmail.com>
2017-11-10 22:13:58 +0530

    work on gs

--- src/pages/daily-teaches-filter/daily-teaches-filter.ts
+++ src/pages/daily-teaches-filter/daily-teaches-filter.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { DailyTeachesPage } from '../daily-teaches/daily-teaches';
+import { AuthService } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the DailyTeachesFilterPage page.
@@ -15,11 +17,95 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DailyTeachesFilterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  studentID:any;
+  filterData: any = {};
+  loading:any;
+  subjects:any= [];
+
+  constructor(public navCtrl: NavController, public params: NavParams, public authService: AuthService, public loadingController: LoadingController) {
+    this.loading = this.loadingController.create({
+      spinner: 'bubbles',
+      content: "Please wait..."
+    });
+    this.loading.present();
+    this.studentID = params.get('studentID');
+    if(params.get('filter')) {
+      this.filterData = params.get('filter');
+    }
+    if(this.filterData.subjects === undefined) {
+      this.filterData.subjects = [];
+    } else if(typeof(this.filterData.subjects) === "string"){
+      let subjects = this.filterData.subjects.split(',').map(Number);
+      if(subjects == 0) {
+        this.filterData.subjects = [];
+      } else {
+        this.filterData.subjects = subjects;
+      }
+    } 
+
+    if(this.filterData.absentee === undefined) {
+      this.filterData.absentee = "all";
+    }
+    
+    if(this.filterData.startDate === undefined) {
+      this.filterData.startDate = "";
+    }
+    if(this.filterData.endDate === undefined) {
+      this.filterData.endDate = "";
+    }
+    this.getFilterData();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DailyTeachesFilterPage');
+  getFilterData() {
+    this.authService.getApiData('daily_teaches/filter_data', {}, this.studentID, this).then((result) => {
+      if(result['subjects']){
+        for(let subject of result['subjects']) {
+          this.subjects.push(subject);
+        }
+      }
+      if(this.loading) {
+        this.loading.dismiss();
+      }
+    });
   }
 
+  apply() {
+    if(this.filterData.startDate === "") {
+      delete(this.filterData.startDate);
+    }
+    if(this.filterData.endDate === "") {
+      delete(this.filterData.endDate);
+    }
+    this.filterData.subjects = this.filterData.subjects.join(',');
+    this.navCtrl.setRoot(DailyTeachesPage, {
+      'studentID': this.studentID,
+      'filter': this.filterData
+    });
+  }
+
+  back() {
+    this.navCtrl.pop();
+  }
+  
+  clear() {
+    this.filterData = {};
+    this.filterData.subjects = [];
+    this.filterData.startDate = "";
+    this.filterData.endDate = "";
+    this.filterData.absentee = "all";
+    this.filterData.examType = "all";
+  }
+
+  isBlankSort() {
+    this.filterData.subjects.length == 0 && this.filterData.startDate == "" && this.filterData.endDate == ""
+  }
+
+  checkSubject(self, subjectId) {
+    if(this.filterData.subjects.includes(subjectId)) {
+      let index = this.filterData.subjects.indexOf(subjectId);
+      this.filterData.subjects.splice(index,1);
+    } else {
+      this.filterData.subjects.push(subjectId);
+    }
+  }
 }
