a17eda5802ce9666d201758d0535a8db6387d836
SwapnilPatil <swapnil.patil04@gmail.com>
2017-12-21 20:27:42 +0530

    work on tos

--- src/pages/new-parent/new-parent.ts
+++ src/pages/new-parent/new-parent.ts
@@ -7,6 +7,7 @@ import { RegisterPage } from '../register/register';
 import { ExamsPage } from '../exams/exams';
 import { AuthService } from '../../providers/auth-service/auth-service';
 import { AlertService } from '../../providers/alert-service/alert-service';
+import { InAppBrowser, InAppBrowserOptions} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-new-parent',
@@ -17,14 +18,23 @@ export class NewParentPage {
   menu:any;
   loading:any;
   
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public menuCtr: MenuController, public authService: AuthService, public alertService: AlertService, public loadingController: LoadingController) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public menuCtr: MenuController, public authService: AuthService, public alertService: AlertService, public loadingController: LoadingController, private inAppBrowser: InAppBrowser) {
     this.menu = menuCtr;
     this.menu.enable(false);
     this.parentForm = formBuilder.group({
       name: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
       mobile: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*'), Validators.required])],
-      confirmMobile: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*'), Validators.required])]
+      confirmMobile: ['', Validators.compose([Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]*'), Validators.required])],
+      agree: [false]
     });
+  }
+
+  tos() {
+    this.inAppBrowser.create('https://eracord.com/terms_of_service', '_system', {});
+  }
+
+  clearFlash() {
+    this.alertService.dismiss();
   }
 
   registerParent() {
@@ -32,8 +42,12 @@ export class NewParentPage {
       this.loading = this.loadingController.create({
         spinner: 'bubbles',
         content: "Please wait..."
-      });  
+      });
       let params = this.parentForm.value;
+      if(params.agree == false) {
+        this.alertService.warning("Please Accept Terms and Conditions");
+        return false;
+      }
       if(params.mobile !== params.confirmMobile) {
         this.alertService.warning("Confirm Mobile does not match!!");
         return true;
