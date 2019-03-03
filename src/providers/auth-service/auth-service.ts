44d2573826ae74648e38ca893f307d4d1ca84424
SwapnilPatil <swapnil.patil04@gmail.com>
2019-03-03 16:42:40 +0530

    work on URL

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -10,10 +10,10 @@ import 'rxjs/add/operator/map';
   and Angular DI.
 */
 //let apiUrl = 'http://localhost:3000/parents/';
-//let apiUrl = 'https://eracord.com/parents/';
-//let serverUrl = 'https://eracord.com/api/v1/parents/';
-let apiUrl = 'http://0.0.0.0:3000/parents/';
-let serverUrl = 'http://0.0.0.0:3000/api/v1/parents/';
+let apiUrl = 'https://eracord.com/parents/';
+let serverUrl = 'https://eracord.com/api/v1/parents/';
+//let apiUrl = 'http://0.0.0.0:3000/parents/';
+//let serverUrl = 'http://0.0.0.0:3000/api/v1/parents/';
 
 @Injectable()
 export class AuthService {
